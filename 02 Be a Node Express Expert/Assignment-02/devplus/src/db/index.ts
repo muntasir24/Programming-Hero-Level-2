965b2319ea2bffd916cc23122ee849c98677e05e
muntasir24 <azizmuntasir@gmail.com>
2026-05-24 12:23:57 +0600

    db table created

--- 02 Be a Node Express Expert/Assignment-02/devplus/src/db/index.ts
+++ 02 Be a Node Express Expert/Assignment-02/devplus/src/db/index.ts
@@ -12,6 +12,29 @@ pool.on("error", (err, client) => {
 const initDB = async () => {
   try {
     await pool.connect();
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        email VARCHAR(255) UNIQUE NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        role VARCHAR(20) NOT NULL DEFAULT 'contributor' CHECK (role IN ('contributor', 'maintainer')),
+        created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+        updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+      );
+    `);
+    await pool.query(`
+      CREATE TABLE  IF NOT EXISTS issues (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(150) NOT NULL,
+  description TEXT NOT NULL,
+  type VARCHAR(20) NOT NULL CHECK (type IN ('bug', 'feature_request')),
+  status VARCHAR(20) NOT NULL DEFAULT 'open' CHECK (status IN ('open', 'in_progress', 'resolved')),
+  reporter_id INTEGER NOT NULL,
+  created_at TIMESTAMPTZ DEFAULT NOW(),
+  updated_at TIMESTAMPTZ DEFAULT NOW()
+);
+  `);
     console.log("Connected to DB");
   } catch (err) {
     console.log("Error connecting to the database:", err);
