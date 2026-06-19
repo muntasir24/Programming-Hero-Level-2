37ca2e82c4337d13da54f86d9af81a2b57dfdd24
muntasir24 <azizmuntasir@gmail.com>
2026-06-19 21:03:49 +0600

    alhamdulillah 7 query done

--- 03 Assignment/QUERY.sql
+++ 03 Assignment/QUERY.sql
@@ -102,6 +102,9 @@ INSERT INTO Bookings (booking_id, user_id, match_id, seat_number, payment_status
 
 
 
+
+
+
 -- Query 1: Retrieve all upcoming football matches belonging to the 'Champions League' where the match status is 'Available'.
 select match_id ,fixture,base_ticket_price from matches
 where tournament_category='Champions League' and match_status='Available'
@@ -110,7 +113,7 @@ where tournament_category='Champions League' and match_status='Available'
 -- Query 2: Search for all users whose full names start with 'Tanvir' or contain the phrase 'Haque' (case-insensitive)
 
 select user_id,full_name,email from users
-where full_name like 'Tanvir%' 
+where full_name Ilike 'Tanvir%' 
   or  full_name Ilike '%haque'
 
 -- Query 3: Retrieve all booking records where the payment status is missing (NULL), replacing the empty result with 'Action Required'.
@@ -125,6 +128,31 @@ select bookings.booking_id , users.full_name, matches.fixture,bookings.total_cos
 from bookings 
 inner join users on users.user_id=bookings.user_id
 inner join matches on matches.match_id=bookings.match_id
+
+
+-- Query 5: Display a comprehensive list of all users and their booking IDs, ensuring that fans who have never bought a ticket are still listed.
+
+select users.user_id, users.full_name,bookings.booking_id
+from users
+left join bookings on bookings.user_id=users.user_id
+
+-- Query 6: Find all ticket bookings where the total cost is strictly higher than the average cost of all ticket bookings.
+
+select booking_id,match_id,total_cost
+from bookings
+where total_cost>(
+select avg(total_cost)
+from bookings
+  );
+
+
+-- Query 7: Retrieve the top 2 most expensive matches sorted by base ticket price, skipping the absolute highest premium match.
+
+
+select match_id	,fixture,	base_ticket_price
+from matches
+order by base_ticket_price desc limit 2 offset 1
+
 
 
 
