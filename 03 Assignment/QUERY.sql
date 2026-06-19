7ee7aa59de6ba14413be406964194a558fd93b6a
muntasir24 <azizmuntasir@gmail.com>
2026-06-19 20:35:01 +0600

    4 query done alhamduillah

--- 03 Assignment/QUERY.sql
+++ 03 Assignment/QUERY.sql
@@ -97,3 +97,35 @@ INSERT INTO Bookings (booking_id, user_id, match_id, seat_number, payment_status
 (504, 2, 101, NULL, NULL, 150.00),
 (505, 3, 102, 'C-20', 'Pending', 120.00);
 
+
+
+
+
+
+-- Query 1: Retrieve all upcoming football matches belonging to the 'Champions League' where the match status is 'Available'.
+select match_id ,fixture,base_ticket_price from matches
+where tournament_category='Champions League' and match_status='Available'
+
+
+-- Query 2: Search for all users whose full names start with 'Tanvir' or contain the phrase 'Haque' (case-insensitive)
+
+select user_id,full_name,email from users
+where full_name like 'Tanvir%' 
+  or  full_name Ilike '%haque'
+
+-- Query 3: Retrieve all booking records where the payment status is missing (NULL), replacing the empty result with 'Action Required'.
+  
+select booking_id ,user_id,match_id ,coalesce(payment_status,'Action Required') as systematic_status from bookings
+where payment_status isnull
+
+-- Query 4: Retrieve match booking details along with the User's full name and the scheduled Match fixture teams.
+
+
+select bookings.booking_id , users.full_name, matches.fixture,bookings.total_cost
+from bookings 
+inner join users on users.user_id=bookings.user_id
+inner join matches on matches.match_id=bookings.match_id
+
+
+
+
